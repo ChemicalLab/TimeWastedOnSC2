e3a723b9f920c7fc8e6d69e2c03d414ab2d7f62f
Ivan Dimitrov <ivanthechemist@gmail.com>
2024-06-11 13:07:37 +0300

    Add requirements, run in parallel, accept folder as an argument, progress bar

--- SC2_time.py
+++ SC2_time.py
@@ -2,17 +2,27 @@ import sc2reader
 import os
 import datetime
 import glob
+import argparse
+from multiprocessing import Pool
+from tqdm import tqdm
 
-total_time_seconds = 0
-total_time = 0
-replays = [f for f in glob.glob("*.SC2Replay")]
+def process_replay(replay_file):
+    replay = sc2reader.load_replay(replay_file, load_level=0)
+    return replay.length.seconds
 
-for each in replays:
-    replay = sc2reader.load_replay(each, load_level=0)
-    total_time_seconds += replay.length.seconds
-    # print(replay.filename)
-    # print(replay.length.seconds)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Calculate the total time of the replays in the specified folder.')
+    parser.add_argument('folder_path', type=str, help='The path to the folder containing the replay files')
 
-total_time = str(datetime.timedelta(seconds=total_time_seconds))
-print(total_time)
-print(total_time_seconds/3600, " hours")
+    args = parser.parse_args()
+
+    replays = [f for f in glob.glob(os.path.join(args.folder_path, "*.SC2Replay"))]
+
+    with Pool() as p:
+        results = list(tqdm(p.imap(process_replay, replays), total=len(replays)))
+
+    total_time_seconds = sum(results)
+    total_time = str(datetime.timedelta(seconds=total_time_seconds))
+
+    print(total_time)
+    print(total_time_seconds/3600, " hours")
